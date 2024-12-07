2b5f11c08e8e0f860c5db242710c2c5846ee908f
Manu Cornet <m@ma.nu>
2024-12-07 09:46:08 +0000

    Simple parsing of config file

--- configparser.ts
+++ configparser.ts
@@ -1,7 +1,19 @@
 import { readFile } from 'fs/promises';
 
-const parseConfig = (fileName) => {
+const parseConfig = async (fileName) => {
   const parsed = {};
+
+  const content = await readFile(fileName, 'utf-8');
+  const lines = content.split('\n');
+
+  for (let line of lines) {
+    line = line.trim()
+    if (!line) {
+      continue;
+    }
+    const parts = line.split(':', 3);
+    parsed[parts[0]] = [parts[1], parts[2]];
+  }
 
   return parsed;
 };
--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@ import parseConfig from './configparser';
 
 import fs from 'fs';
 
-const main = () => {
-  const config = parseConfig('config.txt');
+const main = async () => {
+  const config = await parseConfig('config.txt');
   console.log('Config', config);
 };
 
