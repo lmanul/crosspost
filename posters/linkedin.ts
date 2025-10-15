eed581687a07e736ed883095e77f0b6889d2b26d
Manu Cornet <m@ma.nu>
2025-10-15 16:49:55 +0000

    First step in adding a LinkedIn poster

---
+++ posters/linkedin.ts
@@ -0,0 +1,19 @@
+import Poster from "./poster";
+
+export default class LinkedIn extends Poster {
+    constructor() {
+      super('linkedin', 'https://www.linkedin.com/login');
+    }
+
+    override login = async (page, user, password) => {
+    await page.type('#username', user);
+    await page.type('#password', password);
+    page.keyboard.press('Enter');
+  };
+
+    override loadNewPostPage = async (page) => {
+    const startPostButton = await page.waitForSelector('[aria-label="Start a post"]');
+    await startPostButton.click();
+  };
+
+}
